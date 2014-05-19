3d367600d91462a75630fcf9301e2b6adb66cc6f
Tammy Soo <tammysoo@gmail.com>
2014-05-18 23:58:48 -0700

    caching photos

--- chompsky.py
+++ chompsky.py
@@ -1,90 +1,73 @@
 #!/usr/bin/env python
-from __future__ import print_function
 from flask import Flask, url_for, redirect, render_template, send_file
 from PIL import Image
 from PIL import ImageCms
 import os
 import random
 
-
 app = Flask(__name__)
+
+def check_parameters(width, height):
+    if width < 0 or height < 0:
+        pass
+    if width == 0:
+        width = 75 
+    if height == 0:
+        height = 75 
+    return width, height
+
+def get_path(ratio):
+    if ratio < 1.0:
+        folder = 0
+    elif ratio == 1.0:
+        folder = 1
+    elif ratio > 1.0:
+        folder = 2
+    return "static/images/{0}".format(folder)
+
+def available_photo(file_name):
+    return os.path.isfile(file_name) 
 
 @app.route("/")
 def hello():
     return render_template('index.html') 
 
-@app.route("/<width>/<height>")
-def get_image(width, height):
-    return send_file("static/images/IMG_6612.jpg")
-    
-@app.route("/tshirt/<size>")
-def get_tshirt(size):
-    if size == "small":
-        return send_file("static/images/small.jpg")
-    elif size == "large":
-        return send_file("static/images/large.jpg")
-    else:
-        return "We don't have that size"
-
 @app.route("/chompsky/<int:width>/<int:height>")
 def get_chompsky(width, height):
-    if width < 0 or height < 0:
-        raise PyCMSError
-    elif width == 0 and height == 0:
-        width = 75 
-        height = 75
-    elif width == 0:
-        width = 75 
-    elif height == 0:
-        height = 75 
+    width, height = check_parameters(width, height)     
     ratio = float(width)/height
-    if ratio < 1.0:
-        folder = 0
-    elif ratio == 1.0:
-        folder = 1
-    elif ratio > 1.0:
-        folder = 2
-    path = "static/images/{0}".format(folder)
+    file_name = "{0}_{1}_{2}.png".format(ratio, width, height)
+    path = get_path(ratio)
+   
+    cache_path = "static/images/cache"
+    cached_photo_file = os.path.join(cache_path, file_name) 
+    if available_photo(cached_photo_file):
+        return send_file(cached_photo_file)
+
+    # get photo
     photo = random.choice(os.listdir(path))
     f = Image.open(os.path.join(path, photo))
+    
     (actual_width, actual_height) = f.size
     actual_ratio = float(actual_width)/actual_height
-    required_height = int(actual_width/ratio)
-    required_width = int(actual_height * ratio)
+     
     if ratio > actual_ratio:
+        required_height = int(actual_width/ratio)
         diff_change = (actual_height - required_height)/2
-        box = (0, diff_change, actual_width, actual_height-diff_change)
+        box = (0, diff_change, actual_width, actual_height - diff_change)
         new = f.crop(box)
     elif ratio < actual_ratio:   
+        required_width = int(actual_height * ratio)
         diff_change = (actual_width - required_width)/2
         box = (0 + diff_change, 0, actual_width - diff_change, actual_height)
         new = f.crop(box)
     elif actual_ratio == ratio:
         new = f.resize((width, height))
-    #else:
-        #return "height:{0}, {1}, {2}, width:{3}, {4}, {5}, ratio: {6}".format(required_height, height, actual_height, required_width, width, actual_width, ratio) 
     new = new.resize((width, height))
-    new.save("static/images/new_chompsky.png")
-    return send_file("static/images/new_chompsky.png")
+    file_name = "{0}_{1}_{2}.png".format(ratio, width, height)
+    new_path = os.path.join(cache_path, file_name)
+    new.save(new_path)
+    return send_file(new_path)
     
-
-@app.route("/kris/<width>/<height>")
-def get_kris(width, height):
-    w = int(width)
-    h = int(height)
-    ratio = float(w/h)
-    if ratio < 1.0:
-        folder = 0
-    elif ratio == 1.0:
-        folder = 1
-    elif ratio > 1.0:
-        folder = 2
-    path = "static/images/{0}".format(folder)
-    photo = random.choice(os.listdir(path))
-    f = Image.open(os.path.join(path, photo))
-    new = f.resize((w, h))
-    new.save("static/images/new_chompsky.png")
-    return send_file("static/images/new_chompsky.png")
-
 if __name__ == "__main__":
     app.run(debug=True)
